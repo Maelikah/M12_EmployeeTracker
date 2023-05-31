22a7a3f148f34d6e6cd6361499455275abd44813
Maelikah <eva.longoria@gmail.com>
2023-05-31 14:22:41 -0600

    Created schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+-- Make sure the resources database is removed and created new everytime the schema is sourced
+DROP DATABASE IF EXISTS resources_db;
+CREATE DATABASE resources_db;
+
+-- Make sure to get positioned inside the resources_db database
+USE resources_db;
+
+-- Create department table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- Create role table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+-- Create employee table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);
