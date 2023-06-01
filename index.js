44ae04d42228ed43820a55bd6a791f40d1e885c3
Maelikah <eva.longoria@gmail.com>
2023-06-01 11:55:25 -0600

    Working on switch cases for employee query

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 // Declare variables to hold dependencies
 const asciiArt = require("./lib/ascii");
+const dbConnection = require("./lib/dbConnection");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 console.log(asciiArt);
+dbConnection;
