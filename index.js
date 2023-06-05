4cc38626c493ab245ccda92352aba9054f0252f6
Maelikah <eva.longoria@gmail.com>
2023-06-05 16:15:57 -0600

    Final review

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 
 // Declare variables to hold dependencies
 const asciiArt = require("./lib/ascii");
-const dbConnection = require("./lib/dbConnection");
-const inquirer = require("inquirer");
-const mysql = require("mysql2");
 const promptUser = require("./lib/promptUser");
 
 console.log(asciiArt);
